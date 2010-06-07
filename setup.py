386db3a50aec321f0f51c5015d9eeeba346f887a
Duong Dang <nddang@laas.fr>
2010-06-07 16:54:52 +0200

    back to same old distutils

--- setup.py
+++ setup.py
@@ -1,49 +1,24 @@
-
 #!/usr/bin/env python
-import sys
-#from distutils.core import setup
 
-# setuptools script for simpleparse doesnt install properly, prompt user to
-# install it by hand here
-try:
-    import simpleparse
-except:
-    raise Exception("""
-ERROR: python-simpleparse not found on your system. 
-ERROR: Please resolve this problem first.
-ERROR: You might want to try something like:
-ERROR:   On Ubuntu: sudo apt-get install python-simpleparse
-ERROR: or install from source: http://sourceforge.net/projects/simpleparse/files/simpleparse/2.1.1a2/SimpleParse-2.1.1a2.tar.gz/download
-ERROR: ==========================================================""")
+from distutils.core import setup
 
-try:
-    from setuptools import setup
-except:
-    raise Exception("""
-ERROR: python-setuptools not found on your system. 
-ERROR: Please resolve this problem first.
-ERROR: You might want to try something like:
-ERROR:   On Ubuntu: sudo apt-get install python-setuptools
-ERROR:   On Fedora: sudo yum install python-setuptools
-ERROR: ==========================================================""")
-else:
-    setup(name='robot-viewer',
-          version='1.1.2',
-          license='BSD',
-          platforms='Linux/MacOSX',
-          description='A viewer tool for robots',
-          long_description='A viewer tool for robots',
-          author='Duong Dang',
-          author_email='nddang@laas.fr',
-          url='www.laas.fr/~nddang',
-          packages=['robotviewer',\
-                        'robotviewer.corba',\
-                        'robotviewer.corba.RobotViewer',\
-                        'robotviewer.corba.RobotViewer__POA',\
-                        'robotviewer.corba.hppCorbaServer',\
-                        'robotviewer.corba.hppCorbaServer__POA'],
-          package_dir={'robotviewer':'src'},
-          install_requires=['sphinx >=0.6','pyopengl'],
-         )
+setup(name='robot-viewer',
+      version='1.2.1',
+      license='BSD',
+      platforms='Linux/MacOSX',
+      description='A viewer tool for robots',
+      long_description='A viewer tool for robots',
+      author='Duong Dang',
+      author_email='nddang@laas.fr',
+      url='www.laas.fr/~nddang',
+      packages=['robotviewer',\
+                    'robotviewer.corba',\
+                    'robotviewer.corba.RobotViewer',\
+                    'robotviewer.corba.RobotViewer__POA',\
+                    'robotviewer.corba.hppCorbaServer',\
+                    'robotviewer.corba.hppCorbaServer__POA'],
+      package_dir={'robotviewer':'src'},
+      requires=['sphinx >=0.6','pyopengl'],
+      )
 
 
