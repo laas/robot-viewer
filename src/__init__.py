4a608e38a55f81b0b90ba5ef0ef0938453a029e3
Duong Dang <nddang@laas.fr>
2011-04-19 17:49:12 +0200

    Clean up license text in source code.

--- src/__init__.py
+++ src/__init__.py
@@ -1,52 +1,26 @@
-# ----------------------------------------------------------------------
-# Copyright (c) 2010 Duong Dang
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#  * Neither the name of DarkCoda nor the names of its
-#    contributors may be used to endorse or promote products
-#    derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------
+# Copyright (c) 2010-2011, Duong Dang <mailto:dang.duong@gmail.com>
+# This file is part of robot-viewer.
+
+# robot-viewer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# robot-viewer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with robot-viewer.  If not, see <http://www.gnu.org/licenses/>.
 
 """robotviewer - simple robot visualisation tool
 
-Intended purely as a tool for developers, to quickly display a robot in a
-(or a sequence of) configuration(s).
---------------------
-
-See 'demo.py' in the enclosing directory for a brief tutorial.
-
---------------------
-
-	- Duong Dang
 """
 
 __author__ =  'Duong Dang'
-__email__ = 'nddang@laas.fr'
-__version__=  '0.9.5'
+__email__ = 'dang.duong@gmail.com'
+__version__=  '1.6.1'
 
 from client import client
 
